fc92a9553bb99f679911b7c9baa2377e5be23cae
dianmustofa <mustofa.d@outlook.com>
2025-07-16 23:18:03 +0700

    update api

--- src/lib/quranApi.js
+++ src/lib/quranApi.js
@@ -1,7 +1,8 @@
 // File: /utils/quranApi.js
 
 export async function getAyat(suratId, start, end) {
-  const res = await fetch(`https://api.quran.gading.dev/surah/${suratId}`);
+  // const res = await fetch(`https://api.quran.gading.dev/surah/${suratId}`);
+  const res = await fetch(`https://equran.id/api/surat//${suratId}`);
   const data = await res.json();
 
   if (!data?.data?.verses) {
